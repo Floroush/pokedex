910ac0d1b7ee3d891bbfb9050dc33d7b5a1d87a2
Floroush <florian.araujogruen@gmail.com>
2024-12-04 12:37:21 +0100

    major update: missing second typing not displayed anymore

--- scripts/htmlTemplates.js
+++ scripts/htmlTemplates.js
@@ -1,38 +1,54 @@
 function pokemonContainerHtml(pokemon, pokemonId, i) {
 	return /*html*/ `
-    <div id="${pokemonId}" class="pokemon-container ${
-		pokemon.typing[0] + "-light-shadow"
-	}">
-        <div class="pokemon-header">
-            <div class="pokemon-name">${pokemon.name}</div>
-            <div class="pokemon-types">
-                <div id="pokemonType1" class="icon-small ${pokemon.typing[0]}">
-                    <img src="./assets/icons/${pokemon.typing[0]}.svg">
+    <div id="${pokemonId}" class="pokemon-container ${pokemon.typing[0]}">
+            <section class="pokemon-container__header ${pokemon.typing[0]}">
+                <div class="pokemon-number">${pokemon.number}</div>
+                <div class="pokemon-box">
+                    <div class="pokemon-img">
+                        <img src="${pokemon.sprite}"">
+                    </div>
+                    <div class="pokemon-name">${pokemon.name}</div>
+                    <div class="pokemon-types">
+                        <div>
+                            <div id="pokemonType" class="icon ${
+															pokemon.typing[0]
+														}">
+                                <img src="./assets/icons/${
+																	pokemon.typing[0]
+																}.svg">
+                            </div>
+                        </div>
+                        <div>
+                            <div id="pokemonType" class="icon ${
+															pokemon.typing[1]
+																? pokemon.typing[1]
+																: "no-second-type"
+														}">
+                                <img src="./assets/icons/${
+																	pokemon.typing[1]
+																}.svg">
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div id="pokemonType2" class="icon-small ${pokemon.typing[1]}">
-                    <img src="./assets/icons/${pokemon.typing[1]}.svg">
+            </section>
+            <section class="pokemon-info">
+                <div class="pokemon-info__flex-box">
+                    <div>Species:</div>
+                    <div>?</div>
                 </div>
-            </div>
-            <div class="pokemon-number">${pokemon.number}</div>
-        </div>
-        <div class="pokemon-img ${pokemon.typing[0]} ${
-		pokemon.typing[0] + "-shadow"
-	}">
-            <img src="${pokemon.sprite}">
-        </div>
-        <div class="pokemon-info">
-
-            <div class="pokemon-type">
-        ${pokemon.typing[0]} ${pokemon.typing[1] ? pokemon.typing[1] : ""}
-            </div>
-            <div id="pokemonType" class="icon ${pokemon.typing[0]}">
-                <img src="./assets/icons/${pokemon.typing[0]}.svg">
-            </div>
-            <div id="pokemonType" class="icon ${pokemon.typing[1]}">
-                <img src="./assets/icons/${pokemon.typing[1]}.svg">
-            </div>
-            <div>Height: ${pokemon.height}</div>
-            <div>Weight: ${pokemon.weight}</div>
-        </div>
-    </div>`;
+                <div class="pokemon-info__flex-box">
+                    <div>Height:</div>
+                    <div>${pokemon.height}</div>
+                </div>
+                <div class="pokemon-info__flex-box">
+                    <div>Weight:</div>
+                    <div>${pokemon.weight}</div>
+                </div>
+                <div class="pokemon-info__flex-box">
+                    <div>Abilities:</div>
+                    <div>?</div>
+                </div>
+            </section>
+        </div>`;
 }
